407dfdc52eb787cf82d0d34251bf6e8906f75332
irinabassina <>
2024-06-15 20:01:44 +0300

    added AI analysis dialog

--- internal/ai/chatgpt.go
+++ internal/ai/chatgpt.go
@@ -1,32 +1,80 @@
 package ai
 
 import (
+	"WarehouseTgBot/internal/service"
 	"context"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/sashabaranov/go-openai"
 	"log"
 )
 
+var conversationMessages = make(map[int64][]openai.ChatCompletionMessage)
+
 func GetOpenAIClient(token string) *openai.Client {
 	return openai.NewClient(token)
 }
 
-func AskGPT(openaiClient *openai.Client, update *tgbotapi.Update) (string, error) {
+func AskGPT(ctx context.Context, openaiClient *openai.Client, operationService *service.OperationService,
+	update *tgbotapi.Update) (string, error) {
+
+	message := openai.ChatCompletionMessage{
+		Role:    openai.ChatMessageRoleUser,
+		Content: update.Message.Text,
+	}
+	err := addMessagesContext(ctx, operationService, update.SentFrom().ID, message)
+	if err != nil {
+		return "", err
+	}
+
 	resp, err := openaiClient.CreateChatCompletion(
-		context.Background(),
+		ctx,
 		openai.ChatCompletionRequest{
-			Model: openai.GPT3Dot5Turbo,
-			Messages: []openai.ChatCompletionMessage{
-				{
-					Role:    openai.ChatMessageRoleUser,
-					Content: update.Message.Text,
-				},
-			},
+			Model:    openai.GPT4o,
+			Messages: conversationMessages[update.SentFrom().ID],
 		},
 	)
 	if err != nil {
 		log.Printf("ChatCompletion error: %v\n", err)
 		return "", err
 	}
+	err = addMessagesContext(nil, operationService, update.SentFrom().ID, resp.Choices[0].Message)
+	if err != nil {
+		return "", err
+	}
 	return resp.Choices[0].Message.Content, nil
+}
+
+func addMessagesContext(ctx context.Context, operationService *service.OperationService, userID int64, msg openai.ChatCompletionMessage) error {
+	msgs := conversationMessages[userID]
+	if msgs == nil || len(msgs) == 0 {
+		err := fillDialogStart(ctx, operationService, userID)
+		if err != nil {
+			return err
+		}
+	}
+	conversationMessages[userID] = append(conversationMessages[userID], msg)
+	return nil
+}
+
+func fillDialogStart(ctx context.Context, operationService *service.OperationService, userID int64) error {
+	msgs := []openai.ChatCompletionMessage{
+		{
+			Role:    openai.ChatMessageRoleSystem,
+			Content: "you are a warehouse assistant",
+		},
+	}
+	historyCSV, err := operationService.GetOperationsHistory(ctx)
+	if err != nil {
+		return err
+	}
+	msgs = append(msgs, openai.ChatCompletionMessage{
+		Role:    openai.ChatMessageRoleUser,
+		Content: historyCSV,
+	})
+	conversationMessages[userID] = msgs
+	return nil
+}
+
+func CloseDialog(userID int64) {
+	conversationMessages[userID] = nil
 }
